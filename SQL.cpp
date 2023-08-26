643bd9abd570d7547631c9b2c4503ba64b4b9fc8
SergeyShirmankin <saransk_sega@mail.ru>
2023-08-26 13:02:46 +0300

    enter

--- SQL.cpp
+++ SQL.cpp
@@ -148,76 +148,32 @@ bool  insert_Log_Pass_SQL(char buffer[256],std::string & result)
 		else if (tempRequestProgram.compare("6") == 0) //Авторизация  на лог и пароль
 		{
 			sLgn = objLogPass.get_NameUserSend();
-			strcpy(cLgn, sLgn.c_str());//преооразуем строку в массив char
 			sPswrd = objLogPass.get_PasswordUser();
-			strcpy(cPswrd, sPswrd.c_str());//преооразуем строку в массив char
-			int err = mysql_query(&mysql, "SELECT * FROM _log_pass  WHERE login ='cLgn' AND  password = 'cPswrd'");
-			if (err == 0)
+			std::string isLogPass = "SELECT * FROM _log_pass WHERE login = '" + sLgn + "' AND password  = '" + sPswrd + "'";
+			//std::string isLogPass = "SELECT * FROM _log_pass WHERE login = 'sega' AND password  = '123123'";
+			int errIsLogPass = mysql_query(&mysql, isLogPass.c_str());
+			if ((res = mysql_store_result(&mysql)) && (row = mysql_fetch_row(res)))
 			{
-				if (res = mysql_store_result(&mysql)) {
-					while (row = mysql_fetch_row(res)) {
-						for (i = 0; i < mysql_num_fields(res); i++) {
-							cout << row[i] << "  ";
-						}
-						cout << endl;
-					}
-				}
+				std::cout << " Успешное автоизация  \n";
+				objLogPass.set_CurrentState("7");//Такой логин  уже есть 
+				objLogPass.set_Messaqge("--");//--Создаем пустое сообение 
+				objLogPass.set_NumCurrMess("1");//--номер текущего сообщения
+				objLogPass.set_NumMess("1");//--количество сообщений
+				result = msgForClient();//Сформировать сообщение
+				return true;
+			}
+			else
+			{
+				std::cout << "не успешная автоизация  \n";
+				objLogPass.set_CurrentState("8");//Такой логин  уже есть 
+				objLogPass.set_Messaqge("--");//--Создаем пустое сообение 
+				objLogPass.set_NumCurrMess("1");//--номер текущего сообщения
+				objLogPass.set_NumMess("1");//--количество сообщений
+				result = msgForClient();//Сформировать сообщение
+				return false;
 			}
 		}
-
 	}
-#ifdef  DEBUG_STEP_1
-	mysql_query(&mysql, "SELECT * FROM _log_pass"); //Делаем запрос к таблице
-
-	//Выводим все что есть в таблице через цикл
-	if (res = mysql_store_result(&mysql)) {
-		while (row = mysql_fetch_row(res)) {
-			for (i = 0; i < mysql_num_fields(res); i++) {
-				cout << row[i] << "  ";
-			}
-			cout << endl;
-		}
-	}
-	else
-		cout << "Ошибка MySql номер " << mysql_error(&mysql);
-	// Закрываем соединение с базой данных
 	mysql_close(&mysql);
-#endif
 	system("Pause");
 }
-
-//---------------------------------------------------------------------------------------------------------------------------
-	//------------------------ Создаем таблицу для логинов и паролей-------------------------------------------------------------
-
-	//if (mysql_query(&mysql, "CREATE TABLE _log_pass(id INT AUTO_INCREMENT PRIMARY KEY, login VARCHAR(10),password VARCHAR(10))") == 0)
-	//	std::cout << " Создал базу данных для Логина и пароля\n";
-	//else
-	//	std::cout << "Не смог создать базу данных для Логина и пароля\n";
-
-	//for (int i = 0; i < 3; i++)
-	//{
-	//	if (mysql_query(&mysql, "INSERT INTO _log_pass(id, login, password) values(default,'******','******')") == 0)
-	//		std::cout << " Запись Log Pass  создана \n";
-	//	else
-	//		std::cout << " Запись Logpass не может быть создана \n";
-	//}
-	////-----------------------------------------------------------------------------------------------------------------------------
-	////----------------------------- создаем таблицу для сообщений------------------------------------------------------------------
-
-	//if (mysql_query(&mysql, "CREATE TABLE Messages(id INT AUTO_INCREMENT PRIMARY KEY,own VARCHAR(10),receiver VARCHAR(10),message VARCHAR(200))") == 0)
-	//	std::cout << " Создал базу данных для Логина и пароля\n";
-	//else
-	//	std::cout << "Не смог создать базу для сообщений\n";
-
-	//for (int i = 0; i < 100; i++)
-	//{
-	//	if (mysql_query(&mysql, "INSERT INTO Messages(id, own,receiver,message) values(default,'*******','******','**********')") == 0)
-	//		std::cout << " Запись Message создана \n";
-	//	else
-	//		std::cout << " Запись Message не может быть создана \n";
-	//}
-	//----------------------------------------------------------------------------------------------------------------------------
-		//mysql_query(&mysql, "CREATE TABLE table_fromCplusplus(id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(10))");
-		//mysql_query(&mysql, "INSERT INTO table_fromCplusplus(id, name) values(default, 'Daria')");
-		//mysql_query(&mysql, "INSERT INTO table_fromCplusplus(id, name) values(default, 'Lida')");
-		//mysql_query(&mysql, "INSERT INTO table_fromCplusplus(id, name) values(default, 'Nastya')");
